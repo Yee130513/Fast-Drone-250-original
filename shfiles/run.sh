cf9fdd32760c238c865b68ec9230a3137b6c4baa
Yee130513 <2762496724@qq.com>
2026-07-21 17:04:21 +0800

    config: sync flight startup and disable color stream

---
+++ shfiles/run.sh
@@ -0,0 +1,10 @@
+sudo chmod 777 /dev/ttyACM0 & sleep 2;
+roslaunch realsense2_camera rs_camera.launch & sleep 10;
+roslaunch mavros px4.launch & sleep 10;
+rosrun mavros mavcmd long 511 105 5000 0 0 0 0 0 & sleep 2;
+rosrun mavros mavcmd long 511 31 5000 0 0 0 0 0 & sleep 2;
+roslaunch vins fast_drone_250.launch & sleep 2;
+roslaunch px4ctrl run_ctrl.launch & sleep 2;
+roslaunch ego_planner single_run_in_exp.launch & sleep 2;
+#roslaunch ego_planner rviz.launch & sleep 2;
+wait;
